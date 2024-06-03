65450144fde680e53ba24623d953ab521dba9e3a
ghifarhaidar <ghifarh13@gmail.com>
2024-06-03 18:52:56 +0300

    removed unused models

--- WebTest/Models/PharmacyContext.cs
+++ WebTest/Models/PharmacyContext.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
-namespace WebTest.Models
+namespace DataAccess.Models
 {
     public partial class PharmacyContext : DbContext
     {
